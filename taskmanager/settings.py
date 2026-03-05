e3f0914812f25c2d8dbaa0398a1d495d1ca97f1c
asilios <asilbekakkiyev@gmail.com>
2026-03-05 16:43:44 +0500

    Fix CSRF trusted origins for Railway

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -8,6 +8,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-me-in-producti
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
