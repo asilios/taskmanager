917b5f826e37529677fb7fa4cc474725226e1cc9
asilios <asilbekakkiyev@gmail.com>
2026-03-05 15:23:34 +0500

    Fix flake8 unused imports in tests

--- tests.py
+++ tests.py
@@ -1,7 +1,6 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.contrib.auth.models import User
-from django.urls import reverse
-from tasks.models import Task, Category, Tag
+from tasks.models import Task, Category
 
 
 class AuthTests(TestCase):
